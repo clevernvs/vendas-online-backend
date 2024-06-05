79384c309bf161d3e1626be116e9cfcdf532c497
Cleverton  Neves <clever.nvs@gmail.com>
2024-06-05 16:26:00 -0300

    feat: Insert UserEntity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -11,13 +11,13 @@ export class UserController {
     }
 
     @Get()
-    async getAllUsers(): Promise<User[]> {
+    async getAllUsers(): Promise<UserEntity[]> {
         return this.userService.getAllUsers();
 
     }
 
     @Post()
-    async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
         return this.userService.createUser(createUserDto);
     }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,30 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
 
-    private users: User[] = [];
-
-    async getAllUsers(): Promise<User[]> {
-        return this.users;
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {
     }
 
-    async createUser(createUserDto: CreateUserDto): Promise<User> {
+
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userRepository.find();
+    }
+
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
 
         const saltOrRounds = 10;
         const passwordHashed = await hash(createUserDto.password, saltOrRounds);
 
-        const user: User = {
+        return await this.userRepository.save({
             ...createUserDto,
-            id: this.users.length + 1,
             password: passwordHashed,
-        };
-
-        this.users.push(user);
-
-        return user;
+        });
     }
 }
