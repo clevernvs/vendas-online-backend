49b0d450a8d1ecfad5cb6be1f39d46b28d0cbf07
Cleverton  Neves <clever.nvs@gmail.com>
2024-06-12 18:18:15 -0300

    feat: Inserting create token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,21 @@ import { UserEntity } from 'src/user/entities/user.entity';
 import { LoginDto } from './dtos/login.dto';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { loginPayload } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+        private jwtService: JwtService,
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
+    ) { }
+
+    async login(loginDto: LoginDto): Promise<ReturnLogin> {
         const user: UserEntity | undefined = await this.userService
             .findByEmail(loginDto.email)
             .catch(() => undefined);
@@ -20,7 +28,9 @@ export class AuthService {
             throw new NotFoundException('Email ou password inválido.');
         }
 
-        return user;
-
+        return {
+            accessToken: this.jwtService.sign({ ...new loginPayload(user) }),
+            user: new ReturnUserDto(user),
+        };
     }
 }
